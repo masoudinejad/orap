4ea7b57a0b8688cc4aa93fcdc78ce46a9983bcb0
Mojtaba Masoudinejad <masoudinejad@gmail.com>
2024-10-26 13:29:38 +0200

    add function to get depth of each node

--- orap_dt.py
+++ orap_dt.py
@@ -4,3 +4,15 @@ from sklearn.tree import DecisionTreeClassifier
 class ORAPClassifier(DecisionTreeClassifier):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+
+    def _get_depths(self) -> list[int]:
+        node_count = self.tree_.node_count
+        parent_count = [0] * node_count
+        for i in range(1, node_count):
+            parent = (
+                self.tree_.children_left[i]
+                if self.tree_.children_left[i] != -1
+                else self.tree_.children_right[i]
+            )
+            parent_count[i] = parent_count[parent] + 1
+        self.depths = parent_count
