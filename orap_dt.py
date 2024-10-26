58692f0c980682ee5411bae724a870af23cc7dc8
Mojtaba Masoudinejad <masoudinejad@gmail.com>
2024-10-26 13:38:33 +0200

    explicitly define inputs

--- orap_dt.py
+++ orap_dt.py
@@ -2,8 +2,35 @@ from sklearn.tree import DecisionTreeClassifier
 
 
 class ORAPClassifier(DecisionTreeClassifier):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(
+        self,
+        criterion="gini",
+        splitter="best",
+        max_depth=None,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        min_weight_fraction_leaf=0.0,
+        max_features=None,
+        random_state=None,
+        max_leaf_nodes=None,
+        min_impurity_decrease=0.0,
+        class_weight=None,
+        ccp_alpha=0.0,
+    ) -> None:
+        super().__init__(
+            criterion=criterion,
+            splitter=splitter,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            min_weight_fraction_leaf=min_weight_fraction_leaf,
+            max_features=max_features,
+            random_state=random_state,
+            max_leaf_nodes=max_leaf_nodes,
+            min_impurity_decrease=min_impurity_decrease,
+            class_weight=class_weight,
+            ccp_alpha=ccp_alpha,
+        )
 
     def _get_depths(self) -> list[int]:
         node_count = self.tree_.node_count
